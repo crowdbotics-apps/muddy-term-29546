6e530c79087dbd3657982886fc0fe77de21f4adc
Crowdbotics <team@crowdbotics.com>
2021-08-06 02:56:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "muddy_term_29546Identifier",
+  url: "https://muddy_term_29546Identifier.botics.co",
+  api: "https://muddy_term_29546Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
